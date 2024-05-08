94e419bee678041e09f11205f7787824fe64fa32
Simon Kotchou <simonkotchou@mines.edu>
2024-05-07 23:56:41 -0700

    resnet 18 kan

--- models/KAN/kan_model.py
+++ models/KAN/kan_model.py
@@ -167,3 +167,81 @@ class ConvolutionalKAN(nn.Module):
                         output += self.activation_functions[idx](x[:, j, k:k+x.shape[2]-self.kernel_size+1, l:l+x.shape[3]-self.kernel_size+1])
             outputs.append(output)
         return torch.stack(outputs, dim=1)
+    
+
+class BasicBlock(nn.Module):
+    def __init__(self, in_channels, out_channels, num_bases, degree, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = ConvolutionalKAN(in_channels, out_channels, kernel_size=3, num_bases=num_bases, degree=degree, stride=stride, padding=1)
+        self.bn1 = nn.BatchNorm2d(out_channels)
+        self.relu = nn.ReLU(inplace=True)
+        self.conv2 = ConvolutionalKAN(out_channels, out_channels, kernel_size=3, num_bases=num_bases, degree=degree, stride=1, padding=1)
+        self.bn2 = nn.BatchNorm2d(out_channels)
+        
+        if stride != 1 or in_channels != out_channels:
+            self.shortcut = nn.Sequential(
+                ConvolutionalKAN(in_channels, out_channels, kernel_size=1, num_bases=num_bases, degree=degree, stride=stride),
+                nn.BatchNorm2d(out_channels)
+            )
+        else:
+            self.shortcut = nn.Identity()
+        
+    def forward(self, x):
+        identity = x
+        
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        
+        out = self.conv2(out)
+        out = self.bn2(out)
+        
+        identity = self.shortcut(identity)
+        
+        out += identity
+        out = self.relu(out)
+        
+        return out
+
+class ResNet18KAN(nn.Module):
+    def __init__(self, num_classes, num_bases, degree):
+        super(ResNet18KAN, self).__init__()
+        self.in_channels = 64
+        
+        self.conv1 = ConvolutionalKAN(3, 64, kernel_size=7, num_bases=num_bases, degree=degree, stride=2, padding=3)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.relu = nn.ReLU(inplace=True)
+        self.maxpool = nn.MaxPool2d(kernel_size=3, stride=2, padding=1)
+        
+        self.layer1 = self._make_layer(BasicBlock, 64, 2, num_bases, degree, stride=1)
+        self.layer2 = self._make_layer(BasicBlock, 128, 2, num_bases, degree, stride=2)
+        self.layer3 = self._make_layer(BasicBlock, 256, 2, num_bases, degree, stride=2)
+        self.layer4 = self._make_layer(BasicBlock, 512, 2, num_bases, degree, stride=2)
+        
+        self.avgpool = nn.AdaptiveAvgPool2d((1, 1))
+        self.fc = LinearKAN(512, num_classes, num_bases=num_bases, degree=degree)
+        
+    def _make_layer(self, block, out_channels, num_blocks, num_bases, degree, stride):
+        strides = [stride] + [1] * (num_blocks - 1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_channels, out_channels, num_bases, degree, stride))
+            self.in_channels = out_channels
+        return nn.Sequential(*layers)
+    
+    def forward(self, x):
+        out = self.conv1(x)
+        out = self.bn1(out)
+        out = self.relu(out)
+        out = self.maxpool(out)
+        
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        
+        out = self.avgpool(out)
+        out = torch.flatten(out, 1)
+        out = self.fc(out)
+        
+        return out
